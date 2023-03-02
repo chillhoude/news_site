5b64b07e49d68ea0c23ad92a9a02786bdf9fcd72
Alex <llexa1931@gmail.com>
2023-03-02 22:41:44 +0200

    day1-1

--- Apollo/navigation/urls.py
+++ Apollo/navigation/urls.py
@@ -6,17 +6,18 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',Home.as_view(),name='home'),
     path('tags/',filter_tag,name='tag'),
     path('news',news,name='news'),
     path('news/filter/',filter_date,name='filter'),
-    path('public',public,name='public'),
+    path('public',Public.as_view(),name='public'),
     path('news/filter/<int:pk>',Show_post.as_view(),name='filter_post'),
-    path('blog',blog,name='blog'),
-    path('interview',interview,name='interview'),
+    path('blog',BlogView.as_view(),name='blog'),
+    path('interview',Interview.as_view(),name='interview'),
     path('<int:pk>/',Show_post.as_view(),name='show_post'),
     path('<int:pk>/like/', AddLike.as_view(), name='like'),
     path('<int:pk>/dislike/', AddDislike.as_view(), name='dislike'),
     path('<int:pk>/commentlike/', AddLikeComment.as_view(), name='likecomment'),
-    path('create_post/',create_post,name='create_post')
+    path('create_post/',create_post,name='create_post'),
+    path('userview/<int:pk>',UserDetailView.as_view(),name='user')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
